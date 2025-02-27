bd61d43d40a36bef96d4c238f527d89f08b7c1f2
RobotronicaS <117644697+RobotronicaS@users.noreply.github.com>
2025-02-27 08:35:43 -0300

    app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+async function cargarProductos() {
+    const respuesta = await fetch("data/products.json");
+    const productos = await respuesta.json();
+    const contenedor = document.getElementById("productos");
+
+    contenedor.innerHTML = productos.map(p => `
+        <div>
+            <h2>${p.nombre}</h2>
+            <p>Precio: ${p.precio}</p>
+            <img src="images/${p.imagen}" width="150">
+        </div>
+    `).join("");
+}
+
+window.onload = cargarProductos;
 
